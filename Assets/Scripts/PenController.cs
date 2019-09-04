6aaff89dff7b98d31e8a5193e614c95dbd01f44b
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-09-04 15:16:02 +0200

    implementing movement #beginning

---
+++ Assets/Scripts/PenController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenController : MonoBehaviour
+{
+    private float movementSpeed;
+    private Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * 5.0f, BodyDof.velocity)
+    }
+}
