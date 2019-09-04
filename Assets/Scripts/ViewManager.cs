1d3f3c2d42b8dc283758df13b321adb0c5df2b51
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-09-04 13:14:54 +0200

    Added functionality to buttons: Get Simulation & Create Simulation

---
+++ Assets/Scripts/ViewManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ViewManager : MonoBehaviour
+{
+    //[SerializeField]
+    //private string 
+    //// Start is called before the first frame update
+    //void Start()
+    //{        
+    //}
+
+    public void LoadView(string viewName)
+    {
+        SceneManager.LoadScene(viewName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
