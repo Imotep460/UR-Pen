0834cfba99867b489757a7a504ca436212d7f837
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-09-24 15:28:05 +0200

    added pausebutton script. added pause button to GetPenUniverse & Penuniverse, comment spellcheck, added Waypoint model class and waypointDatabase.cs so that a List<> of waypoints can be created. Hardcoded 2 waypoints in WaypointDatabase.cs, for testingputposes.

--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Simulation : MonoBehaviour
 {
+    [SerializeField]
+    private TMPro.TextMeshProUGUI positionText;
+    [SerializeField]
+    private TMPro.TextMeshProUGUI rotationText;
+
     // Create collection/array of waypoints
     [SerializeField]
     private Transform[] waypoints;
@@ -12,7 +18,7 @@ public class Simulation : MonoBehaviour
     // Placeholder for the current targetrotation
     private Quaternion targetRotation;
 
-    // Create a float to modiy the movementspeed of the gameobject.
+    // Create a float to modify the movementspeed of the gameobject.
     [SerializeField]
     private float movementSpeed;
 
@@ -20,18 +26,20 @@ public class Simulation : MonoBehaviour
     [SerializeField]
     private float rotationSpeed;
 
-    // Create a int to switch between the waypoints in Tranform[] waypoints.
+    // Create a int to switch between the waypoints in Transform[] waypoints as well as indicate which waypoint the Player Object is moving to.
     private int waypointIndex;
 
     // Start is called before the first frame update
     void Start()
     {
+        //// Get a path where we can safely store data.
+        //Debug.Log(Application.persistentDataPath);
         // Set the first waypoint to move to.
-        // Set the first waypoint position.
+        // Set the first waypoint.position.
         targetPosition = waypoints[0].position;
-        // Set the first waypoint rotation.
+        // Set the first waypoint.rotation.
         targetRotation = waypoints[0].rotation;
-        // make sure the waypointIndex starts a 0.
+        // make sure the waypointIndex starts at 0.
         waypointIndex = 0;
     }
 
@@ -42,7 +50,13 @@ public class Simulation : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementSpeed * Time.deltaTime);
         // Rotate towards the targetRotation.
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-        // Check if the distance between the gameObjects current position is within a certain distance of the targetPosition.
+
+        // Print out the current position of the pen tip. Updates every frame.
+        positionText.text = string.Format("Position:\n{0}", transform.position);
+        // Print ot the current rotation of the pen tip. Updates every frame
+        rotationText.text = string.Format("Rotation:\n{0}", transform.rotation);
+
+        // Check if the distance between the gameObjects current position is within a certain distance of the targetPosition before moving on to the next waypoint.
         if (Vector3.Distance(transform.position, targetPosition) < .01)
         {
             // Check to make sure the waypointIndex is NOT biger than the amount of waypoints in Transform[] waypoints.
